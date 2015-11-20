f58fe81f8ff98f3b29699985c5e98f051ca01082
Francesco Viglino <kawashita@hotmail.it>
2015-11-20 19:32:56 +0100

    branches-test

--- symfony/recipes/cron.rb
+++ symfony/recipes/cron.rb
@@ -4,7 +4,6 @@ node[:deploy].each do |app_name, deploy|
     elsif platform?("amazon")
         user = "apache"
     end
-
     if node[app_name].present? and node[app_name][:crons].present?
         node[app_name][:crons].each do |cron|
             if cron[:symfony].present? and !cron[:symfony]
